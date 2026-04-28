cd370bd784c2c953ff6d1d20820b30f81ba3f2f6
Luan Santos <luan.santoosz@outlook.com.br>
2026-04-28 19:15:47 -0300

    Delete lab3/projeto2/gamesapi directory

--- lab3/projeto2/gamesapi/src/main/java/org/example/GameRepository.java
+++
@@ -1,7 +0,0 @@
-package org.example;
-
-import org.example.Game;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GameRepository extends JpaRepository<Game, Long> {
-}
